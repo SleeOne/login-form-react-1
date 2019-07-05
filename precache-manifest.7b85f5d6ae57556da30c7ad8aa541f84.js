8d7cf407e93a0d30c0bb4a6cd3b5f2299667ba29
SleeOne <krevetko253@gmail.com>
2019-07-05 13:05:25 +0300

    Updates

---
+++ precache-manifest.7b85f5d6ae57556da30c7ad8aa541f84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3362760ae80a220500b5d4e3cf6f567d",
+    "url": "/login-form-react-1/index.html"
+  },
+  {
+    "revision": "fd598e86dc24cefa5caf",
+    "url": "/login-form-react-1/static/css/main.4e3db060.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/login-form-react-1/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "fd598e86dc24cefa5caf",
+    "url": "/login-form-react-1/static/js/main.ec2082e4.chunk.js"
+  },
+  {
+    "revision": "20e8a616d07f9edc2ada",
+    "url": "/login-form-react-1/static/js/runtime~main.dd4696f1.js"
+  }
+]);
